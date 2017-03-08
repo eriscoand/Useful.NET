e44ab3121eea46d3f03493da03840e1c290b8528
Eric Risco <eric.risco@andornet.ad>
2017-03-08 17:56:33 +0100

    Added toUpperWithoutSpaces (matricules)

--- CleanStrings.cs
+++ CleanStrings.cs
@@ -13,9 +13,19 @@ public static class CleanStrings
         str = System.Text.RegularExpressions.Regex.Replace(str, @"\s", "-"); // //Replace spaces by dashes
         return str;
     }
+
+    public static string toUpperWithoutSpaces(string s)
+    {
+        string str = RemoveAccent(s).ToLower();
+        str = System.Text.RegularExpressions.Regex.Replace(str, @"[^a-z0-9\s-]", ""); // Remove all non valid chars          
+        str = System.Text.RegularExpressions.Regex.Replace(str, @"\s+", "").Trim(); // convert multiple spaces into zero space  
+        return str;
+    }
+
     public static string RemoveAccent(string txt)
     {
         byte[] bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
         return System.Text.Encoding.ASCII.GetString(bytes);
     }
+
 }
